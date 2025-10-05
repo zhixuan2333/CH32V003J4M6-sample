89de0b234528005e42b71abd52926e2dbe46dc24
zhixuan2333 <me@zhixuan.dev>
2025-10-05 12:30:38 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,42 @@
+# CH32V003J4M6 Sample Code
+
+## BOM
+- [CH32V003J4M6](https://akizukidenshi.com/catalog/g/g118062/)
+- [SOP8 to DIP8 Adapter](https://akizukidenshi.com/catalog/g/g105154/)
+- [WCH-LinkE](https://akizukidenshi.com/catalog/g/g118065/)
+- Below is optional
+    - LED
+    - Push Button
+    - etc...
+
+## pin out
+![pinout](https://qiita-user-contents.imgix.net/https%3A%2F%2Fqiita-image-store.s3.ap-northeast-1.amazonaws.com%2F0%2F111210%2F18d5bc5e-325c-edfb-d6e4-cfa114412d27.png?ixlib=rb-4.0.0&auto=format&gif-q=60&q=75&s=ead8630cc17d4958d2bb588ed380d08a)
+> from qitta user content
+
+## Samples
+
+- GPIO
+    - digital read with pullup
+    - digital write (SDK)
+    - digital write (register)
+- stanby
+    - AWU
+    - GPIO EXTI
+- EXTI
+    - PIN Falling
+
+## Serial
+
+I remap the UART TX to PA1(Pin 1).
+Connect PA1 to a USB to RX pin.
+- Baudrate: 115200
+
+
+## Reference
+
+
+- [WCH](https://www.wch-ic.com/)
+    - [Reference Manual](https://www.wch-ic.com/downloads/CH32V003RM_PDF.html)
+    - [Datasheet](https://www.wch-ic.com/downloads/CH32V003DS0_PDF.html)
+- [Community-PIO-CH32V/platform-ch32v](https://github.com/Community-PIO-CH32V/platform-ch32v)
+- [wagiminator/Development-Boards](https://github.com/wagiminator/Development-Boards)
--- src/main.c
+++ src/main.c
@@ -1,65 +1,130 @@
-#include <debug.h>
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2023/12/22
+ * Description        : Main program body.
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
 
-void EXTI_INT_INIT(void)
+/*
+ *@Note
+ *External interrupt line routine:
+ *EXTI_Line0(PD0)
+ *PD0 sets the pull-up input, and the falling edge triggers the interrupt.
+ *
+ */
+
+#include "debug.h"
+
+/* Global define */
+
+/* Global Variable */
+
+/*********************************************************************
+ * @fn      EXTI0_INT_INIT
+ *
+ * @brief   Initializes EXTI0 collection.
+ *
+ * @return  none
+ */
+void EXTI0_INT_INIT(void)
 {
-    EXTI_InitTypeDef EXTI_InitStructure = {0};
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-
-    GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource2);
-    EXTI_InitStructure.EXTI_Line = EXTI_Line2;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Event;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
-
-    NVIC_InitTypeDef NVIC_InitStructure = {0};
-    NVIC_InitStructure.NVIC_IRQChannel = EXTI7_0_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-}
-
-
-int main(void) {
-
-  Delay_Init();            // Initialize delay function
-  Delay_Ms(5000);        // wait 5s for debugger connection
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-    SystemCoreClockUpdate();
-
-    EXTI_INT_INIT();
-
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); // Enable GPIOC clock
   GPIO_InitTypeDef GPIO_InitStructure = {0};
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1; // Pin 13
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // Push-pull output
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; // Speed 50
-  GPIO_Init(GPIOC, &GPIO_InitStructure); // Initialize GPIOC with the settings
+  EXTI_InitTypeDef EXTI_InitStructure = {0};
+  NVIC_InitTypeDef NVIC_InitStructure = {0};
 
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOC, ENABLE);
 
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOC, &GPIO_InitStructure); // Initialize GPIOC with the settings
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  /* GPIOC ----> EXTI_Line2 */
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource2);
+  EXTI_InitStructure.EXTI_Line = EXTI_Line2;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStructure);
+
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI7_0_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
 
 
-  // RCC_LSICmd(ENABLE);
-  // while(RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET);
-  // // LSI is 128kHz
-  // PWR_AWU_SetPrescaler(PWR_AWU_Prescaler_10240);
-  // PWR_AWU_SetWindowValue(10); // x [ms]
-  // PWR_AutoWakeUpCmd(ENABLE);
+uint64_t counter = 0;
 
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void)
+{
 
-  while(1) {
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+  SystemCoreClockUpdate();
+  Delay_Init();
+#if (SDI_PRINT == SDI_PR_OPEN)
+  SDI_Printf_Enable();
+#else
+  USART_Printf_Init(115200);
+#endif
+  printf("SystemClk:%d\r\n", SystemCoreClock);
+  printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID());
+  printf("EXTI0 Test\r\n");
+  EXTI0_INT_INIT();
 
-    GPIOC->OUTDR ^= GPIO_Pin_1; // Toggle the LED on/off
-    Delay_Ms(100);                // wait 100ms
-    PWR_EnterSTANDBYMode(PWR_STANDBYEntry_WFE);
-
+  while (1)
+  {
+    counter++;
+    // printf("Run at main\r\n", counter);
+    Delay_Ms(1);
   }
 }
 
+
+void EXTI7_0_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+
+/*********************************************************************
+ * @fn      EXTI0_IRQHandler
+ *
+ * @brief   This function handles EXTI0 Handler.
+ *
+ * @return  none
+ */
+void EXTI7_0_IRQHandler(void)
+{
+  static uint64_t last_counter = 0;
+  if (EXTI_GetITStatus(EXTI_Line2) != RESET)
+  {
+    // print last and current
+    printf("last_counter:%d\r\n", last_counter);
+    printf("counter:%d\r\n", counter);
+
+    if (counter - last_counter < 500)
+    {
+      // Debounce: ignore if less than 500 counts since last
+      EXTI_ClearITPendingBit(EXTI_Line2); /* Clear Flag */
+      return;
+    }
+    last_counter = counter;
+    printf("Run at EXTI\r\n");
+    GPIOC->OUTDR ^= GPIO_Pin_1;
+    EXTI_ClearITPendingBit(EXTI_Line2); /* Clear Flag */
+  }
+}
